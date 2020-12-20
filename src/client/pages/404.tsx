efbf6c874cd6996a49bac09c52980ba34565f4e5
Johannes Katajamäki <jkatajamki@gmail.com>
2020-12-20 20:55:50 +0200

    Feat: Add 404 page

---
+++ src/client/pages/404.tsx
@@ -0,0 +1,10 @@
+const Custom404Page = () => (
+  <>
+    <section className="content flex-content">
+      <h1>Page not found - 404</h1>
+      <p>Sorry - this page could not be found!</p>
+    </section>
+  </>
+)
+
+export default Custom404Page
