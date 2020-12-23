2cf42da212a85cecc39800383d59afd2c676ed31
Johannes Katajamäki <jkatajamki@gmail.com>
2020-12-23 19:33:43 +0200

    Fix: Don't expose user secret via users API

--- server/white_lights/src/users/wl_users.rs
+++ server/white_lights/src/users/wl_users.rs
@@ -1,7 +1,7 @@
 use chrono::{NaiveDateTime};
 use crate::schema;
 use crate::db;
-use crate::diesel::RunQueryDsl;
+use crate::diesel::{QueryDsl, RunQueryDsl};
 use serde::Serialize;
 
 #[derive(Queryable, Serialize)]
@@ -9,8 +9,12 @@ pub struct WLUser {
     pub user_id: i64,
     pub created_at: NaiveDateTime,
     pub email: String,
-    pub user_secret: String,
     pub updated_at: NaiveDateTime,
+}
+
+pub struct CreateUserRequest {
+    pub email: String,
+    pub user_secret: String,
 }
 
 pub fn db_get_users() -> Result<Vec<WLUser>, diesel::result::Error> {
@@ -18,5 +22,12 @@ pub fn db_get_users() -> Result<Vec<WLUser>, diesel::result::Error> {
 
     let connection = db::db_connect();
 
-    wl_users.load::<WLUser>(&connection)
+    wl_users
+        .select((
+            user_id,
+            created_at,
+            email,
+            updated_at,
+        ))
+        .load::<WLUser>(&connection)
 }
